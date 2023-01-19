c318fb28a0f23ee06e6bde1fbe2bce18818bc8a3
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-01-19 05:27:18 +0100

    Fixed error

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	msg := sayHello("Alice")
-    fmt.Println(msg)
+	fmt.Println(msg)
 }
 
 func sayHello(name string) string {
-    return fmt.Sprintf("Hi %s", name)
+	// Change this to "Hello %s" instead of "Hi %s".
+	return fmt.Sprintf("Hello %s", name)
 }
